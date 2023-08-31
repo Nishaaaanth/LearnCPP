388447bbf299c1d45de3cb3d41f33ab9e0a03be5
Nishanth Kiran <nishanthkiran2@gmail.com>
2023-09-01 00:32:25 +0530

    docs: Operators till positive number output formatiing

--- learn.cpp
+++ learn.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <iomanip>
+#include <compare>
 using namespace std;
 
 int main(void) {
@@ -122,11 +123,119 @@ int main(void) {
 		auto a{123u};  => unsigned
 		auto a{123ul}; => unsigned long
 		auto a{123ll}; => long long
+
+		//auto is dangerous when we have to assign different value to it later
+		auto var3{22u};
+		var3 = -2;       => instead of this compiler will put in garbage values here
 	*/
 
-	char value{65};
-	std::cout<<value<<std::endl;
-	std::cout<<static_cast<char>(static_cast<int>(value)+5)<<std::endl;
+	/* OPERATORS
+		Addition        => +
+		Subtraction     => -
+		Multiplication  => *
+		Division        => /
+		Modulus         => %
+	*/
+
+	/* PRECEDENCE & ASSOCIATIVITY
+		Precedence     => what operation to do first
+		Associativity  => what direction or order
+	*/
+
+	/* PREFIX & POSTFIX INCREMENT/DECREMENT
+		int value{5};
+		value++;
+		value--;
+
+		++value;
+		--value;
+	*/
+
+	/* COMPOUND ASSIGNEMENT OPERATORS
+		+=
+		-=
+		*=
+		/=
+		%=
+	*/
+
+	/* COMPARATOR OPERATOR
+		==
+		!=
+		>=
+		<=
+		>
+		<
+		<=>  : spaceship operator (compares 3 way)
+
+		To print bool we can use the same operator
+		std::cout<<std::boolalpha;
+
+		If we want to print the evaluation of an expression directly through cout, we'll need to have ().
+
+		cout<<(a < b)<<endl;
+
+		If we remove () it'll evaluate '<<' first due to higher precedence causing error.
+	*/
+
+	/* LOGICAL OPERATORS
+		a && b => AND
+		a || b => OR
+		!a     => NOT
+	*/
+	
+	/* OUTPUT FORMATTING
+		Libraries:
+		#include<ios>
+		#include<iomanip>
+
+		1. endl  => to print new line character
+		2. flush => causes immediate sending of data to the device connected to the stream. So, what happens with 'cout' is that the data goes to a intermediary buffer(like a storage) before printing in the console. It'll first print out each word and line one by one to that buffer and then directly transfer all of the data to the console at once.
+		std::cout<<std::flush;
+		std::cout<<"Hello there\nHow's it going there?"<<endl;
+
+		3. setw() => to set the width of the value to be printed.
+		std::cout << std::setw(10) << "Firstname" <<std::setw(10)<<"Lastname"<< std::setw(5) <<"Age"<<std::endl;
+		std::cout << std::setw(10) << "Nishanth" <<std::setw(10)<<"Kiran"<< std::setw(5) <<"23"<<std::endl;
+		std::cout << std::setw(10) << "someone" <<std::setw(10)<<"else"<< std::setw(5) <<"20"<<std::endl;
+		
+		4. right  => to tell that after setting width to our data it should print to the right side of the formatted layout
+		std::cout<<std::right;
+		std::cout << std::setw(10) << "Firstname" <<std::setw(10)<<"Lastname"<< std::setw(5) <<"Age"<<std::endl;
+
+		5. left  => to tell that after setting width to our data it should print to the left side of the formatted layout
+		std::cout<<std::left;
+		std::cout << std::setw(10) << "Firstname" <<std::setw(10)<<"Lastname"<< std::setw(5) <<"Age"<<std::endl;
+
+		6. internal  => to tell that after setting width to our data it should print to the right most side and the negative sign if the value is negative should print on the left most side of the formatted layout
+		std::cout<<std::internal;
+		std::cout << std::setw(10) << "Firstname" <<std::setw(10)<<"Lastname"<< std::setw(5) <<"Age"<<std::endl;
+
+		7. setfill()  => to tell the console to print the value provided inside the function to be printed instead of spaces after each value in the formatted output
+		std::cout<<std::internal;
+		std::cout<<std::setfill('*');
+		std::cout << std::setw(10) << "Firstname" <<std::setw(10)<<"Lastname"<< std::setw(5) <<"Age"<<std::endl;
+		
+		8. boolalpha => to print the boolean values instead of 0 and 1
+		std::cout<<std::boolalpha;
+		std::cout<<(true)<<std::endl;
+
+		9. noboolalpha => to remove the implementation of 'boolalpha'
+		std::cout<<std::boolalpha;
+		std::cout<<(true)<<std::endl;
+		std::cout<<std::noboolalpha;
+		std::cout<<(true)<<std::endl;
+
+		10. showpos  => to show + sign if the value is positive. But, - will always be shown for negative numbers by default
+		std::cout<<std::showpos;
+		std::cout<<(34)<<std::endl;
+
+		11. noshowpos  => to remove the implementation of 'showpos'.
+		std::cout<<std::showpos;
+		std::cout<<(34)<<std::endl;
+		std::cout<<std::noshowpos;
+		std::cout<<(34)<<std::endl;
+	*/
 
 	return 0;
 }
